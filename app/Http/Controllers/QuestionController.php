a5eacda10c75f407f0dfc2a7f8a5cc12fdc8740c
Raphael Garcia <raphael.henriquegarcia@gmail.com>
2024-02-28 00:52:49 -0300

    hey-6: should check if ends with question mark ?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Http\{RedirectResponse, Request};
 
 class QuestionController extends Controller
 {
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $attributes = $request->validate([
-            'question' => ['min:10']
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if ($value[strlen($value) - 1] !== '?') {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                    }
+                },
+            ],
         ]);
 
         Question::query()->create($attributes);
